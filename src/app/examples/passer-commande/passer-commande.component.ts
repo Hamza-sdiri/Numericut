4b4023bf02a68df16e5ff65838750e1d787081e6
Mezni Oubeid <oubeid.mezni@esprit.tn>
2024-09-23 22:59:58 +0100

    Refactor login and signup links in login and signup components

--- src/app/examples/passer-commande/passer-commande.component.ts
+++ src/app/examples/passer-commande/passer-commande.component.ts
@@ -1,70 +1,93 @@
-import { Component, OnInit } from '@angular/core';
-import { ComandeService } from 'app/services/comande.service';
-import { MachineService } from 'app/services/machine.services';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ComandeService } from "app/services/comande.service";
+import { ChangeDetectorRef } from "@angular/core"; // Import ChangeDetectorRef
+import { MachineService } from "app/services/machine.services";
 
 @Component({
-  selector: 'app-passer-commande',
-  templateUrl: './passer-commande.component.html',
-  styleUrls: ['./passer-commande.component.css']
+  selector: "app-passer-commande",
+  templateUrl: "./passer-commande.component.html",
+  styleUrls: ["./passer-commande.component.css"],
 })
 export class PasserCommandeComponent implements OnInit {
-
   newComande = {
-    customer: '',
-    totalPrice: 0,
-    shippingAddress: '',
-    paymentMethod: '',
-    orderStatus: 'processing', 
+    customer: "",
+    totalPrice: null,
+    shippingAddress: "",
+    paymentMethod: "",
+    orderStatus: "processing",
     orderedAt: new Date(),
-    deliveredAt: null
+    deliveredAt: null,
+    machine: null,
   };
+
   machines: any;
 
+  constructor(
+    private comandeService: ComandeService,
+    private machineService: MachineService,
+    private router: Router,
+    private cdr: ChangeDetectorRef // Inject ChangeDetectorRef
+  ) {}
+
   ngOnInit(): void {
-    const username = localStorage.getItem('username')
+    const username = localStorage.getItem("username");
     if (username) {
       this.newComande.customer = username;
     }
     this.loadMachines();
   }
 
-  constructor(private comandeService: ComandeService, private machineService:MachineService) { }
-
-
-  loadMachines(){
+  loadMachines() {
     this.machineService.getAllMachines().subscribe(
-      data => {
+      (data) => {
         this.machines = data;
       },
-      error => {
-        console.error('Error fetching machine list', error);
+      (error) => {
+        console.error("Error fetching machine list", error);
       }
-    )
+    );
   }
 
+  onMachineSelect(machineId: string): void {
+    const selectedMachine = this.machines.find(
+      (machine) => machine._id === machineId
+    );
+    if (selectedMachine) {
+      this.newComande.machine = selectedMachine.name;
+      this.newComande.totalPrice = selectedMachine.price; // Update total price based on selected machine
+      this.cdr.detectChanges(); // Manually trigger change detection
+    }
+  }
 
   onSubmit(): void {
     this.comandeService.createComande(this.newComande).subscribe(
-      response => {
-        console.log('Order created successfully', response);
+      (response) => {
+        console.log("Order created successfully", response);
         // Optionally reset the form or provide feedback to the user
         this.resetForm();
       },
-      error => {
-        console.error('Error creating order', error);
+      (error) => {
+        console.error("Error creating order", error);
       }
     );
   }
 
   resetForm(): void {
     this.newComande = {
-      customer: '',
+      customer: "",
       totalPrice: 0,
-      shippingAddress: '',
-      paymentMethod: '',
-      orderStatus: 'processing',
+      shippingAddress: "",
+      paymentMethod: "",
+      orderStatus: "processing",
       orderedAt: new Date(),
-      deliveredAt: null
+      deliveredAt: null,
+      machine: null,
     };
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(["/login"]);
   }
 }
